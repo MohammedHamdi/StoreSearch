099941f2d79d924a6ad015ace84f067e4251675d
Mohammed Hamdi <mhah000@hotmail.com>
2019-11-24 12:41:28 +0200

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Mohammed Hamdi on 11/24/19.
+//  Copyright © 2019 Mohammed Hamdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
