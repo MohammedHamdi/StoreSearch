6a202f7d08b7cd1dca6a1ed8f25ef0ffb5bd88e7
Mohammed Hamdi <mhah000@hotmail.com>
2019-11-24 16:30:57 +0200

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Mohammed Hamdi on 11/24/19.
+//  Copyright © 2019 Mohammed Hamdi. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
